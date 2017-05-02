463b633bef43099c50f4a0f372577ab0c28a39ce
LiYizhan <1062585732@qq.com>
2017-05-03 00:16:26 +0800

    LoginFragment Update.

--- app/src/main/java/com/sweetorangejuice/artisan/view/SignInFragment.java
+++ app/src/main/java/com/sweetorangejuice/artisan/view/SignInFragment.java
@@ -1,5 +1,6 @@
 package com.sweetorangejuice.artisan.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,23 +10,41 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.sweetorangejuice.artisan.R;
 
 /**
- * Created by as on 2017/5/2.
+ * Author 李易沾
+ * Created by 李易沾 in 2017/5/2
+ * Function:登录和注册的Fragment
  */
 
 public class SignInFragment extends Fragment {
 
-    private TextView mSkipTextView;
-    private EditText mSignInAccountEditText;
-    private EditText mSignInPasswordEditText;
+    //判断登录还是注册的值,false代表登录，true代表注册
+    private boolean mLoginStatus;
+
+    //非登录或注册视图
     private Button mSignInButton;
     private Button mSignUpButton;
+    private TextView mSkipTextView;
+
+    //以下为登录的视图
+    private LinearLayout mSignInLinearLayout;
+    private EditText mSignInAccountEditText;
+    private EditText mSignInPasswordEditText;
     private CheckBox mSignInRememberAccountCheckBox;
+    private Button mLoginSignInButton;
+
+    //以下为注册的视图
+    private LinearLayout mSignUpLinearLayout;
+    private EditText mSignUpAccountEditText;
+    private EditText mSignUpPasswordEditText;
+    private EditText mSignUpEmailEditText;
+    private Button mLoginSignUpButton;
+
 
     @Nullable
     @Override
@@ -33,37 +52,57 @@ public class SignInFragment extends Fragment {
         View view=inflater.inflate(R.layout.fragment_sign_in,container,false);
 
         //绑定视图
+        mSignInLinearLayout=(LinearLayout)view.findViewById(R.id.sign_in_linear_layout);
         mSkipTextView=(TextView)view.findViewById(R.id.sign_in_skip_text_view);
         mSignInAccountEditText=(EditText)view.findViewById(R.id.sign_in_account_edit_text);
         mSignInPasswordEditText=(EditText)view.findViewById(R.id.sign_in_password_edit_text);
         mSignInButton=(Button)view.findViewById(R.id.sign_in_button);
         mSignUpButton=(Button)view.findViewById(R.id.sign_up_button);
         mSignInRememberAccountCheckBox=(CheckBox)view.findViewById(R.id.sign_in_remember_account_check_box);
+        mLoginSignInButton=(Button)view.findViewById(R.id.login_sign_in_button);
+        mSignUpLinearLayout=(LinearLayout)view.findViewById(R.id.sign_up_linear_layout);
+        mSignUpAccountEditText=(EditText)view.findViewById(R.id.sign_up_account_edit_text);
+        mSignUpPasswordEditText=(EditText)view.findViewById(R.id.sign_up_password_edit_text);
+        mSignUpEmailEditText=(EditText)view.findViewById(R.id.sign_up_email_edit_text);
+        mLoginSignUpButton=(Button)view.findViewById(R.id.login_sign_up_button);
 
-
-        //测试，以下正式发布时注释掉
-        mSkipTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(),"跳过 记住账号："+mSignInRememberAccountCheckBox.isChecked(),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+        //监听事件
         mSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(),"登录 账号："+mSignInAccountEditText.getText().toString(),
-                        Toast.LENGTH_SHORT).show();
+                updateLoginUI(false);
             }
         });
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(),"注册 密码："+mSignInPasswordEditText.getText().toString(),
-                        Toast.LENGTH_SHORT).show();
+                updateLoginUI(true);
             }
         });
 
+        //首次打开，显示登陆视图
+        updateLoginUI(false);
+
         return view;
+    }
+
+
+    /**
+     * updateLoginUI 当用户点击登录或者注册按钮的时候，更新各自视图
+     * @param loginStatus 现在的登录或者注册状态
+     */
+    private void updateLoginUI(boolean loginStatus){
+        mLoginStatus=loginStatus;
+        if(!mLoginStatus){
+            mSignInLinearLayout.setVisibility(View.VISIBLE);
+            mSignUpLinearLayout.setVisibility(View.GONE);
+            mSignInButton.setBackgroundColor(0xFF6495ED);
+            mSignUpButton.setBackgroundColor(Color.TRANSPARENT);
+        }else{
+            mSignInLinearLayout.setVisibility(View.GONE);
+            mSignUpLinearLayout.setVisibility(View.VISIBLE);
+            mSignInButton.setBackgroundColor(Color.TRANSPARENT);
+            mSignUpButton.setBackgroundColor(0xFF6495ED);
+        }
     }
 }
